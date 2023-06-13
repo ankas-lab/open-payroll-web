8932c3b321a7bae3529b36bc0ddf1d8b43edd62c
Tomas Rawski <rtomas@gmail.com>
2023-06-13 12:03:36 -0300

    get contract balance and format it.

--- src/components/contracts/contractRow.tsx
+++ src/components/contracts/contractRow.tsx
@@ -9,9 +9,10 @@ import { Archivo } from 'next/font/google';
 const archivo = Archivo({ subsets: ['latin'] });
 
 import { useContract, useCall, useBlockHeader, useApi } from 'useink';
-import { pickDecoded } from 'useink/utils';
+import { BN, pickDecoded } from 'useink/utils';
 
 import metadata from '@/contract/open_payroll.json';
+import { parse } from 'path';
 
 interface ContractRowProps {
   contract: {
@@ -33,7 +34,6 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
   const [contractBalance, setContractBalance] = useState<null | number>(null);
   const [nextBlockPeriod, setNextBlockPeriod] = useState<null | number>(null);
   const [fundsNeeded, setFundsNeeded] = useState<null | string>(null);
-  const [periodicity, setPeriodicity] = useState<any | null>(null);
   const [state, setState] = useState<boolean | undefined>(undefined);
 
   //---------------------------------Api---------------------------------
@@ -46,13 +46,12 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
 
   const getNextBlockPeriod = useCall(_contract, 'getNextBlockPeriod');
 
-  const getContractBalance = useCall(_contract, 'getContractBalance');
+  const getContractBalance = useCall<string>(_contract, 'getContractBalance');
 
   const getTotalDebts = useCall(_contract, 'getTotalDebts');
 
   // 🤟🤟🤟 Get periodicity from contract
-  const getPeriodicity = useCall(_contract, 'getPeriodicity');
-
+  const getPeriodicity = useCall<number>(_contract, 'getPeriodicity');
 
   //---------------------------------Initialize functions---------------------------------
   useEffect(() => {
@@ -66,6 +65,12 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
     }
   }, [_contract]);
 
+  useEffect(() => {
+    if (_contract) {
+      console.log(getContractBalance);
+    }
+  }, [getContractBalance.result?.ok]);
+
   //---------------------------------Truncate numbers-------------------------------------
   function trunc(x: number, p = 0) {
     var s = x.toString();
@@ -75,8 +80,29 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
     return Number(numStr);
   }
 
-  return loading === 'done' ? (
+  function formatBalance(balance: string | undefined): number | undefined {
+    // return undefined if balance is undefined so can continue with the spinner waiting
+    if (balance === undefined) return undefined;
 
+    let num_string = balance.replace(/,/g, '.').replace(/[^0-9.]/g, '');
+    let num = parseInt(num_string);
+
+    return trunc(Math.pow(num * 10, 2), 2);
+  }
+
+  function formatBalanceWithSymbol(balance: number | undefined): string | undefined {
+    if (balance === undefined) return undefined;
+
+    let tokenSymbol = chainInfo?.tokenSymbol?.toString() || '';
+    return `${balance} ${tokenSymbol}`;
+  }
+
+  let contractBalanceFormat = formatBalanceWithSymbol(
+    formatBalance(pickDecoded(getContractBalance.result!)?.toString()),
+  );
+  let periodicityValue = pickDecoded(getPeriodicity.result!)?.toString();
+
+  return loading === 'done' ? (
     <tr
       className={
         i % 2 === 0
@@ -97,8 +123,8 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
         )}
       </td>
       <td className="w-[80px]">
-        {periodicity ? (
-          <p>{getPeriodicity.result?.ok}</p>
+        {periodicityValue ? (
+          <p>{periodicityValue}</p>
         ) : (
           <div className="flex items-center w-full">
             <AiOutlineLoading className="animate-spin" />
@@ -106,18 +132,8 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
         )}
       </td>
       <td className="w-[80px]">
-        {contractBalance !== null ? (
-          <p className="text-ellipsis overflow-hidden">
-            {trunc(
-              Math.pow(
-                contractBalance * 10,
-                // parseInt(chainInfo?.tokenDecimals[0])
-                2, //TODO remove this
-              ),
-              2,
-            )}{' '}
-            {chainInfo?.tokenSymbol?.toString()}
-          </p>
+        {contractBalanceFormat !== null ? (
+          <p className="text-ellipsis overflow-hidden">{contractBalanceFormat}</p>
         ) : (
           <div className="flex items-center w-full">
             <AiOutlineLoading className="animate-spin" />
@@ -164,7 +180,6 @@ const ContractRow = ({ contract, i }: ContractRowProps) => {
         <IoIosAlert className="w-5 h-5 text-opdanger" />
       </td>
     </tr>
-
   ) : (
     <div className="flex items-center w-full">
       <AiOutlineLoading className="w-5 h-5 animate-spin mx-auto my-2" />
