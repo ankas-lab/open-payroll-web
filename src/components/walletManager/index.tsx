35a8444ebec35264a95836ca936f2724bf60bda4
0xLucca <95830307+0xLucca@users.noreply.github.com>
2023-06-14 13:17:32 -0300

    Edit V1 (#3)

--- src/components/walletManager/index.tsx
+++ src/components/walletManager/index.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { useWallet } from "useink";
-import { FaAngleDown, FaAngleUp } from "react-icons/fa";
-import { Archivo } from "next/font/google";
-const archivo = Archivo({ subsets: ["latin"] });
+import React, { useState } from 'react';
+import { useWallet } from 'useink';
+import { FaAngleDown, FaAngleUp } from 'react-icons/fa';
+import { Archivo } from 'next/font/google';
+const archivo = Archivo({ subsets: ['latin'] });
 
 const WalletManager = () => {
   const { account, accounts, setAccount, connect, disconnect } = useWallet();
@@ -16,27 +16,16 @@ const WalletManager = () => {
     <div className="flex absolute">
       <div className="pl-4 pr-2 py-3 border-oppurple border-2 rounded-[5px] flex ml-auto mt-6 w-full md:w-fit h-fit bg-[#FFFFFF] justify-between md:justify-normal z-50">
         <div className="flex flex-col">
-          <p className={show ? "mb-[10px]" : ""}>
+          <p className={show ? 'mb-[10px]' : ''}>
             {account?.name} ({account?.address.slice(0, 5)}...
-            {account?.address.slice(
-              account?.address.length - 5,
-              account?.address.length
-            )}
-            )
+            {account?.address.slice(account?.address.length - 5, account?.address.length)})
           </p>
           {show && (
             <div className="flex flex-col gap-[10px] border-t-2 border-t-oppurple">
               {accounts?.map((a) => (
-                <button
-                  className="rounded hover:bg-opwhite p-1.5 text-left"
-                  onClick={() => handleAccount(a)}
-                >
+                <button className="rounded hover:bg-opwhite p-1.5 text-left" onClick={() => handleAccount(a)}>
                   {a?.name} ({account?.address.slice(0, 5)}...
-                  {account?.address.slice(
-                    account?.address.length - 5,
-                    account?.address.length
-                  )}
-                  )
+                  {a?.address.slice(a?.address.length - 5, a?.address.length)})
                 </button>
               ))}
               <button
@@ -48,10 +37,7 @@ const WalletManager = () => {
             </div>
           )}
         </div>
-        <div
-          className="cursor-pointer px-4 mt-1.5"
-          onClick={() => setShow(!show)}
-        >
+        <div className="cursor-pointer px-4 mt-1.5" onClick={() => setShow(!show)}>
           {show ? <FaAngleUp /> : <FaAngleDown />}
         </div>
       </div>
--- src/pages/edit/[edit].tsx
+++ src/pages/edit/[edit].tsx
@@ -1,118 +1,86 @@
-import React, { useEffect, useState } from "react";
-import Nav from "../../components/nav";
-import Text from "../../components/generals/text";
-import Button from "../../components/generals/button";
-import { Archivo, Podkova } from "next/font/google";
-import { useRouter } from "next/router";
-import {
-  useApi,
-  useBlockHeader,
-  useCall,
-  useContract,
-  useWallet,
-} from "useink";
-import WalletManager from "@/components/walletManager";
-import { pickDecoded } from "useink/utils";
-const podkova = Podkova({ subsets: ["latin"] });
-const archivo = Archivo({ subsets: ["latin"] });
-import metadata from "../../contract/open_payroll.json";
+import React, { useEffect, useState } from 'react';
+import Nav from '../../components/nav';
+import Text from '../../components/generals/text';
+import Button from '../../components/generals/button';
+import { Archivo, Podkova } from 'next/font/google';
+import { useRouter } from 'next/router';
+import { useApi, useBlockHeader, useCall, useContract, useWallet } from 'useink';
+import WalletManager from '@/components/walletManager';
+import { pickDecoded } from 'useink/utils';
+const podkova = Podkova({ subsets: ['latin'] });
+const archivo = Archivo({ subsets: ['latin'] });
+import metadata from '../../contract/open_payroll.json';
 
-import Contract from "@/components/edit/baseContract";
-import Multipliers from "@/components/edit/multipliers";
-import Beneficiaries from "@/components/edit/beneficiaries";
+import Contract from '@/components/edit/baseContract';
+import Multipliers from '@/components/edit/multipliers';
+import Beneficiaries from '@/components/edit/beneficiaries';
+import { AiOutlineLoading } from 'react-icons/ai';
 
 export default function Edit() {
-  //---------------------------------Security---------------------------------
-  const router = useRouter();
-  const { account } = useWallet();
-  useEffect(() => {
-    !account && router.push("/");
-  }, [account]);
-
   //---------------------------------Get contract address---------------------------------
+  const router = useRouter();
   const {
     query: { edit },
   } = router;
   //---------------------------------UseStates---------------------------------
-  const [loading, setLoading] = useState<"loading" | "done" | "error">(
-    "loading"
-  );
+  const [loading, setLoading] = useState<'loading' | 'done' | 'error'>('loading');
+  const [tab, setTab] = useState<string>('contract');
+  const [contractAddress, setContractAddress] = useState<string>('');
+  //---------------------------------Get contract---------------------------------
+  const _contract = useContract(contractAddress, metadata);
 
   // Tabs
-  const [tab, setTab] = useState<string>("contract");
 
-  /*
-  const handleContractChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    const { name, value } = event.target;
-    setNewContract((prevContract) => ({
-      ...prevContract,
-      [name]: value,
-    }));
-  };
-*/
+  useEffect(() => {
+    if (edit?.toString()) {
+      setContractAddress(edit?.toString());
+    }
+  }, [edit]);
+
+  useEffect(() => {
+    if (_contract !== undefined) {
+      setLoading('done');
+      //console.log(_contract);
+    }
+  }, [_contract]);
 
   return (
     <main className={`flex flex-col md:flex-row ${archivo.className}`}>
       <Nav />
-      <div className="w-10/12 md:w-8/12 overflow-x-scroll min-h-screen mx-auto flex flex-col gap-[20px] md:gap-[40px] mt-[50px] md:mt-[0px]">
-        <div className="hidden md:flex h-[100px] justify-end">
-          <WalletManager />
+      {loading === 'loading' ? (
+        <div className="flex w-full min-h-screen">
+          <AiOutlineLoading className="animate-spin m-auto" />
         </div>
-        <div className="flex flex-col justify-between md:w-10/12 lg:w-7/12">
-          <Text type="h2" text="Edit contract" />
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-[10px]">
-            {tab === "contract" ? (
-              <Button
-                type="active"
-                text="contract"
-                icon=""
-                action={() => setTab("contract")}
-              />
-            ) : (
-              <Button
-                type="outlined"
-                text="contract"
-                icon=""
-                action={() => setTab("contract")}
-              />
-            )}
-            {tab === "multipliers" ? (
-              <Button
-                type="active"
-                text="multipliers"
-                icon=""
-                action={() => setTab("multipliers")}
-              />
-            ) : (
-              <Button
-                type="outlined"
-                text="multipliers"
-                icon=""
-                action={() => setTab("multipliers")}
-              />
-            )}
-            {tab === "beneficiaries" ? (
-              <Button
-                type="active"
-                text="beneficiaries"
-                icon=""
-                action={() => setTab("beneficiaries")}
-              />
-            ) : (
-              <Button
-                type="outlined"
-                text="beneficiaries"
-                icon=""
-                action={() => setTab("beneficiaries")}
-              />
-            )}
+      ) : (
+        <div className="w-10/12 md:w-8/12 overflow-x-scroll min-h-screen mx-auto flex flex-col gap-[20px] md:gap-[40px] mt-[50px] md:mt-[0px]">
+          <div className="hidden md:flex h-[100px] justify-end">
+            <WalletManager />
           </div>
+          <div className="flex flex-col justify-between md:w-10/12 lg:w-7/12">
+            <Text type="h2" text="Edit contract" />
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-[10px]">
+              {tab === 'contract' ? (
+                <Button type="active" text="contract" icon="" action={() => setTab('contract')} />
+              ) : (
+                <Button type="outlined" text="contract" icon="" action={() => setTab('contract')} />
+              )}
+              {tab === 'multipliers' ? (
+                <Button type="active" text="multipliers" icon="" action={() => setTab('multipliers')} />
+              ) : (
+                <Button type="outlined" text="multipliers" icon="" action={() => setTab('multipliers')} />
+              )}
+              {tab === 'beneficiaries' ? (
+                <Button type="active" text="beneficiaries" icon="" action={() => setTab('beneficiaries')} />
+              ) : (
+                <Button type="outlined" text="beneficiaries" icon="" action={() => setTab('beneficiaries')} />
+              )}
+            </div>
+          </div>
+          {tab === 'contract' && <Contract _contract={_contract} />}
+          {tab === 'multipliers' && <Multipliers />}
+          {tab === 'beneficiaries' && <Beneficiaries />}
         </div>
-        {tab === "contract" && <Contract address={edit!.toString()} />}
-        {tab === "multipliers" && <Multipliers />}
-        {tab === "beneficiaries" && <Beneficiaries />}
-      </div>
+      )}
     </main>
   );
 }
