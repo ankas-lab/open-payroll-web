74208e8a3a63a91202be19b231275ec7dad287ec
0xLucca <0xlucca.dev@gmail.com>
2023-06-15 10:44:32 -0300

    wip: Update claim page

--- src/pages/claim/index.tsx
+++ src/pages/claim/index.tsx
@@ -1,25 +1,71 @@
-import React, { useEffect, useState } from "react";
-import Nav from "../../components/nav";
-import Text from "../../components/generals/text";
-import Button from "../../components/generals/button";
-import { Archivo } from "next/font/google";
-import { useRouter } from "next/router";
-const archivo = Archivo({ subsets: ["latin"] });
-import { useWallet } from "useink";
-import WalletManager from "@/components/walletManager";
+import React, { useEffect, useState } from 'react';
+import Nav from '../../components/nav';
+import Text from '../../components/generals/text';
+import Button from '../../components/generals/button';
+import { Archivo } from 'next/font/google';
+import { useRouter } from 'next/router';
+const archivo = Archivo({ subsets: ['latin'] });
+import { useWallet, useApi } from 'useink';
+import WalletManager from '@/components/walletManager';
 
 export default function Claim() {
+  const codeHash = "0x9e248273f592c02032d8d7509cd70fe9aeba42f3d3cdbe54c0bae0f8a37b72f7";
   const router = useRouter();
   const { account } = useWallet();
+  const api = useApi('rococo-contracts-testnet');
+
   useEffect(() => {
-    !account && router.push("/");
+    !account && router.push('/');
   }, [account]);
 
-  const [address, setAddress] = useState<string>("");
+  const [contractAddress, setContractAddress] = useState<string>('');
+  const [validContractAddress, setValidContractAddress] = useState<boolean>(false);
 
-  const handleFindContract = (address: string) => {
-    console.log(address);
+  const handleFindContract = async (contractAddress: string) => {
+    console.log(contractAddress);
+    if (api) {
+      try
+      {
+        const res = await api.api.query.contracts.contractInfoOf(contractAddress);
+        let result = res.toHuman();
+        console.log(result);
+        if (result !== null) {
+            if(result.codeHash === codeHash){
+              router.push(`/claim/${contractAddress}`);
+            }
+            //TODO: Show error
+        }
+        else{
+          //TODO: Show error
+        }
+      }
+      catch (e)
+      {
+        console.log(e);
+      }
+    }
   };
+
+  const handleContractAddressChange = (newContractAddress:any) => {
+    setContractAddress(newContractAddress);
+    if (api) {
+      try
+      {
+        if(newContractAddress.length === 0){
+          throw new Error("invalid_address");
+        }
+        
+        const formattedAddress = api.api.registry.createType('AccountId',newContractAddress);
+        console.log(formattedAddress);
+        setValidContractAddress(true);
+      }
+      catch (e)
+      {
+        setValidContractAddress(false);
+        console.log("invalid_address");
+      }
+    }
+  }
 
   return (
     <main className={`flex flex-col md:flex-row ${archivo.className}`}>
@@ -31,35 +77,22 @@ export default function Claim() {
         <Text type="h2" text="Contracts you can claim" />
         <div className="max-w-[400px] flex flex-col gap-[10px]">
           <Text type="h4" text="Find new contract" />
-          <Text
-            type=""
-            text="Enter the address of the contract to claim your payment"
-          />
+          <Text type="" text="Enter the contractAddress of the contract to claim your payment" />
           <form className="flex flex-col gap-[10px]">
             <input
-              id="address"
+              id="contractAddress"
               type="text"
-              name="address"
-              placeholder="Contract address"
+              name="contractAddress"
+              placeholder="Contract contractAddress"
               className="bg-opwhite border-2 border-oppurple rounded-[5px] py-1.5 px-1.5"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
+              value={contractAddress}
+              onChange={(e) => handleContractAddressChange(e.target.value)}
             />
             <div className="flex">
-              {address.length > 0 ? (
-                <Button
-                  type="active"
-                  text="find"
-                  icon=""
-                  action={() => handleFindContract(address)}
-                />
+              {validContractAddress ? (
+                <Button type="active" text="find" icon="" action={() => handleFindContract(contractAddress)} />
               ) : (
-                <Button
-                  type="disabled"
-                  text="find"
-                  icon=""
-                  action={() => handleFindContract(address)}
-                />
+                <Button type="disabled" text="Invalid address" icon="" />
               )}
             </div>
           </form>
@@ -72,7 +105,7 @@ export default function Claim() {
                 <Text type="overline" text="name" />
               </th>
               <th className="w-[300px] flex">
-                <Text type="overline" text="address" />
+                <Text type="overline" text="contractAddress" />
               </th>
             </tr>
             <tr className="flex items-center">
