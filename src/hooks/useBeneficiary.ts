3ea3b2295eb084519d50e26594d50674e1ed19eb
Gera Barboni <63870012+geraBarboni@users.noreply.github.com>
2023-06-18 17:50:06 -0300

    update hook useBeneficiary

--- src/hooks/useBeneficiary.ts
+++ src/hooks/useBeneficiary.ts
@@ -9,6 +9,8 @@ export function useBeneficiary(address: string, contract: ChainContract<any> | u
   const [amountToClaim, SetAmountToClaim] = useState<undefined | any>(undefined);
   const [lastClaim, setLastClaim] = useState<undefined | any>(1111);
   const [beneficiary, setBeneficiary] = useState<any | undefined>(undefined);
+  const [beneficiaryMultipliers, setBeneficiaryMultipliers] = useState<undefined | any>(undefined);
+  const [beneficiaryUnclaimedPayments, setBeneficiaryUnclaimedPayments] = useState<undefined | any>(undefined);
 
   //---------------------------------Api---------------------------------
   const api = useApi('rococo-contracts-testnet');
@@ -25,18 +27,21 @@ export function useBeneficiary(address: string, contract: ChainContract<any> | u
   }, [contract]);
 
   useEffect(() => {
-    if (getAmountToClaim.result) {
+    if (getAmountToClaim.result?.ok) {
       let data = stringNumberToBN(pickResultOk(getAmountToClaim.result!)!);
       SetAmountToClaim(planckToDecimalFormatted(data, api?.api));
     }
-  }, [getAmountToClaim.result]);
+  }, [getAmountToClaim.result?.ok]);
 
   useEffect(() => {
     if (getBeneficiary.result?.ok) {
       let data = pickResultOk(getBeneficiary.result!);
       setBeneficiary(data);
+      setLastClaim(data.last_claim);
+      setBeneficiaryMultipliers(data.multipliers);
+      setBeneficiaryUnclaimedPayments(data.unclaimed_payments);
     }
   }, [getBeneficiary.result?.ok]);
 
-  return { beneficiary, amountToClaim, lastClaim };
+  return { beneficiary, amountToClaim, lastClaim, beneficiaryMultipliers, beneficiaryUnclaimedPayments };
 }
