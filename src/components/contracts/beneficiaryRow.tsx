fdd22c0ac752ba7b960e3c7511a6456373ce29c3
Gera Barboni <63870012+geraBarboni@users.noreply.github.com>
2023-06-20 12:42:48 -0300

    show multipliers

--- src/components/contracts/beneficiaryRow.tsx
+++ src/components/contracts/beneficiaryRow.tsx
@@ -13,6 +13,8 @@ import { pickDecoded, planckToDecimalFormatted } from 'useink/utils';
 import { BN } from 'bn.js';
 import { useBeneficiary, usePayrollContract } from '@/hooks';
 
+import MultiplierCell from '@/components/contracts/multiplerCell';
+
 import { DappContext } from '@/context';
 
 interface BeneficiarieRowProps {
@@ -23,8 +25,11 @@ interface BeneficiarieRowProps {
 
 const BeneficiaryRow = ({ beneficiaryAddress, indexBeneficiary, contract }: BeneficiarieRowProps) => {
   const blockHeader = useBlockHeader();
-  const { amountToClaim, beneficiary } = useBeneficiary(beneficiaryAddress, contract);
-  const { basePayment } = usePayrollContract(contract);
+  const { amountToClaim, beneficiary, beneficiaryMultipliersToArray, finalPay } = useBeneficiary(
+    beneficiaryAddress,
+    contract,
+  );
+  const { multipliersList } = usePayrollContract(contract);
 
   const context = useContext(DappContext);
 
@@ -41,41 +46,13 @@ const BeneficiaryRow = ({ beneficiaryAddress, indexBeneficiary, contract }: Bene
   //---------------------------------UseStates---------------------------------
   const [loading, setLoading] = useState<'loading' | 'done' | 'error'>('loading');
 
-  //---------------------------------Set in states---------------------------------
-  /* const seeBeneficiary = async () => setBeneficiary(pickDecoded(await getBeneficiary.send([_beneficiary])));
-
-  const seeBasePayment = async () => {
-    const basePayment = pickDecoded(await getBasePayment.send());
-    basePayment !== undefined && setBasePayment(parseInt(basePayment.replace(/,/g, '')));
-  };
-
-  const seeAmountToClaim = async () => {
-    const amountToClaim = pickDecoded(await getAmountToClaim.send([_beneficiary]));
-    setAmountToClaim(parseInt(amountToClaim?.Ok.replace(/,/g, '')));
-  };
-  const seePeriodicity = async () => {
-    const periciodicty = pickDecoded(await getPeriodicity.send());
-    setPeriodicity(parseInt(periciodicty.replace(/,/g, '')));
-  };
-
-  const calculateTotalMultipliers = () => {
-    const multipliers = Object.values(beneficiary?.Ok.multipliers);
-    let multipliersSum;
-    multipliers.length === 0
-      ? (multipliersSum = 1)
-      : (multipliersSum = multipliers.reduce((acumulator: number, value: any) => {
-          return acumulator + parseInt(value);
-        }, 0));
-    return multipliersSum;
-  }; */
-
   //---------------------------------Initialize functions---------------------------------
 
   useEffect(() => {
-    if (amountToClaim) {
+    if (contract) {
       setLoading('done');
     }
-  }, [contract, amountToClaim]);
+  }, [contract]);
 
   return loading === 'done' ? (
     <tr
@@ -94,16 +71,24 @@ const BeneficiaryRow = ({ beneficiaryAddress, indexBeneficiary, contract }: Bene
         <p>{addressToShort(beneficiaryAddress)}</p>
       </td>
       {/* Multipliers */}
-      {Object.values(beneficiary?.multipliers).map((m: any) => (
-        <td className="w-[100px]">
-          <p>{m === '0' ? '1' : m}</p>
-        </td>
+      {multipliersList?.map((m: any) => (
+        <MultiplierCell
+          key={m}
+          contract={contract}
+          mult={m}
+          beneficiaryMultipliersToArray={beneficiaryMultipliersToArray}
+        />
       ))}
-
       {/* Final pay */}
-      <td className="w-[100px]">
-        <p>{basePayment}</p>
-      </td>
+      {finalPay !== undefined ? (
+        <td className="w-[100px]">
+          <p>{finalPay}</p>
+        </td>
+      ) : (
+        <td className="w-[100px]">
+          <AiOutlineLoading className="w-5 h-5 animate-spin mx-auto my-2" />
+        </td>
+      )}
       {/* Total to claim */}
       <td className="w-[100px]">
         <p>{amountToClaim}</p>
@@ -114,7 +99,7 @@ const BeneficiaryRow = ({ beneficiaryAddress, indexBeneficiary, contract }: Bene
           {/* {beneficiary !== null &&
             trunc(parseInt(beneficiary?.Ok.lastUpdatedPeriodBlock.replace(/,/g, '')) / periodicity / 7200)} */}
         </p>
-      </td>
+      </td>{' '}
     </tr>
   ) : (
     <div className="flex items-center w-full">
